79f753d45b3db536ef9f0f79096d2ec71ed9e68b
Rohan <rohan@Rohans-MacBook-Air.local>
2024-12-07 21:00:59 +0530

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        mono: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        text: "#dff1f1",
+        background: "#071111",
+        primary: "#9bd4d1",
+        secondary: "#653274",
+        accent: "#a6c34e",
+      },
+    },
+  },
+  plugins: [],
+};
