832c4d84fc3c5ab0b178b4bc00397bbae89780cb
amrohan <rohanxsalunkhe@gmail.com>
2025-09-06 19:23:21 +0530

    feat: add mobile nav

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink, ThemeToggle, RouterLinkActive],
+  imports: [RouterLink, ThemeToggle, RouterLinkActive, NgClass],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {}
+export class Nav {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+}
