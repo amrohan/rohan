9578407120f70d09a61385f6a972ad9881a74574
amrohan <rohanxsalunkhe@gmail.com>
2025-09-15 19:15:29 +0530

    feat: add title resolver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,31 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
 import { Home } from './home/home';
+import {titleResolver} from './core/title-resolver';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+    title: titleResolver,
   },
   {
     path: 'project',
     loadComponent: () => import('./project/project').then((m) => m.Project),
+    title: titleResolver,
   },
   {
     path: 'work',
     loadComponent: () => import('./work/work').then((m) => m.Work),
+    title: titleResolver,
   },
   {
     path: 'contact',
     loadComponent: () => import('./contact/contact').then((m) => m.Contact),
+    title: titleResolver,
   },
   {
     path: 'guestbook',
     loadComponent: () => import('./guestbook/guestbook').then((m) => m.Guestbook),
+    title: titleResolver,
   },
 ];
---
+++ src/app/core/title-resolver.ts
@@ -0,0 +1,11 @@
+import { ResolveFn } from '@angular/router';
+
+export const titleResolver: ResolveFn<string> = (route ) => {
+  const segments = route.url.map(s => s.path);
+  const firstSegment = segments[0] ?? 'Home';
+
+  const capitalized =
+    firstSegment.charAt(0).toUpperCase() + firstSegment.slice(1);
+
+  return `${capitalized} | Rohan Salunkhe`;
+};
