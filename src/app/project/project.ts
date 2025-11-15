3f4e450d50b2cf46d94a5d622bed57ea82ab4176
amrohan <rohanxsalunkhe@gmail.com>
2025-11-15 19:40:38 +0530

    chore: update project stats

--- src/app/project/project.ts
+++ src/app/project/project.ts
@@ -34,7 +34,7 @@ export class Project {
       liveDemoUrl: '',
       articleUrl: 'https://github.com/amrohan/termix/blob/main/README.md',
       tags: ['.NET', 'C#', 'CLI', 'Spectre.Console', 'Developer Tool'],
-      downloadCount: 3.5,
+      downloadCount: 3.6,
       packageUrl: 'https://www.nuget.org/packages/Termix/',
       year: 2025,
     },
