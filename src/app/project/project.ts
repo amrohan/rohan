98369247b5b3f60fb41dab14e28dadeb5fcf55d8
amrohan <rohanxsalunkhe@gmail.com>
2026-02-02 18:59:45 +0530

    chore: bump verison of termix download

--- src/app/project/project.ts
+++ src/app/project/project.ts
@@ -34,7 +34,7 @@ export class Project {
       liveDemoUrl: '',
       articleUrl: 'https://github.com/amrohan/termix/blob/main/README.md',
       tags: ['.NET', 'C#', 'CLI', 'Spectre.Console', 'Developer Tool'],
-      downloadCount: 3.9,
+      downloadCount: 4.0,
       packageUrl: 'https://www.nuget.org/packages/Termix/',
       year: 2025,
     },
